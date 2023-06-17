10b9eb45d2617e2fac46241222fedf528c37d8f3
AZX <3310698819@qq.com>
2023-06-18 03:27:22 +0800

    V1

--- Workplace/1-3_4IO扩展（串转并）/main.c
+++ Workplace/1-3_4IO扩展（串转并）/main.c
@@ -1,3 +1,6 @@
+/*************8*8LED点阵以一行循环滚动显示
+              LED点阵旁的J24黄色跳线帽短接到GND一端
+*************/
 #include <REGX51.H>
 
 
@@ -8,45 +11,66 @@
 typedef unsigned char u8;
 typedef unsigned int  u16;
 
-sbit SRCLK=P3^6;
-sbit RCLK=P3^5;
-sbit SER=P3^4;
+sbit SRCLK=P3^6;//移位寄存器时钟输入
+sbit rCLK=P3^5; //存储寄出去时钟输入
+sbit SER=P3^4;  //串行数据输入
 
-##define LEDDZ_COL_PORT P0
+#define LEDDZ_COL_PORT P0//点阵控制端口
+
+u8 ghc595_buf[8]={0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};//0000 0001|0000 0010|0000 0100|0000 1000|0001 0000|0010 0000|0100 0000|1000 0000
 void delay_10us (u16 ten_us)
 {
 	while (ten_us--);
     
 }
+/*******************************************************************************
+* 函 数 名       : delay_ms
+* 函数功能		 : ms延时函数，ms=1时，大约延时1ms
+* 输    入       : ten_us
+* 输    出    	 : 无
+*******************************************************************************/
+//void delay_ms(u16 ms)
+//{
+//	u16 i,j;
+//	for(i=ms;i>0;i--)
+//		for(j=110;j>0;j--);
+//}
 /*595发送函数*/
+/*向74HC595写入一个字节的数据*/
 
 void hc595_write_data (u8 dat)
 {
 	u8 i=0;
-	for(i=0;i<8;i++)
+	for(i=0;i<8;i++)			//循环8次即可将一个字节写入寄存器中
     {
 		
-		SER=dat>>7;
+		SER=dat>>7;				//优先传输一个字节中的高位
 		dat<<=1;   				//左移1位，次高位移到最高位
 		SRCLK=0;
 		delay_10us(1);			//时序微秒级，更好兼容芯片
 		SRCLK=1;
-		delay_10us(1);
+		delay_10us(1);//移位寄存器时钟上升沿将端口数据送入寄存器中
 			    
 	
 	}
-	RCLK=0;
+	rCLK=0;
 	delay_10us(1);
-	RCLK=1;
+	rCLK=1;//存储寄存器时钟上升沿将前面写入到寄存器的数据输出
 }
 
 
 void main()
 {
-	u8 key=0;
+	u8 i=0;
+	LEDDZ_COL_PORT=0x00;//一开始置低电平  将LED点阵列全部设置为0，即LED阴极为低电平
      while (1)
        {
-	
+			for(i=0;i<8;i++)
+				{
+					hc595_write_data(0x00);//消影 消除前面寄存器缓存数据
+					hc595_write_data (ghc595_buf[i]);//消除前面寄存器缓存数据
+					delay_10us (5000);
+				}
        }
 
 
@@ -222,3 +246,4 @@ void main()
 //			SMG_A_DP_PORT=gsmg_code[key-1];//得到的按键值减1换算成数组下标对应0-F段码		
 //	}		
 //}
+}
